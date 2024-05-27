ee54db1b0fddaedd65788d7d4e198d3d3f7d4eab
IvayloManev1 <134681817+IvayloManev1@users.noreply.github.com>
2024-05-27 10:56:18 +0300

    game engine DONE

---
+++ ArenaGame/Heroes/Monk.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Heroes
+{
+    public class Monk : Hero
+    {
+        private double probability;
+        public double Probability { get { return probability; } }
+        public Monk(string name, double armor, double strenght, IWeapon weapon) : base(name, armor, strenght, weapon)
+        {
+            probability = random.NextDouble();
+        }
+
+        public override double Attack()
+        {
+            double damage = base.Attack();
+            
+            if (probability < 0.30)
+            {
+                //StunStatus += 2;
+                return damage;
+            }
+
+            else return damage;
+
+        }
+
+        public override double Defend(double damage)
+        {
+            double probability = random.NextDouble();
+            if (probability < 0.30)
+            {
+                return 0;
+            }
+
+            else return base.Defend(damage);
+        }
+    }
+}
---
+++ ArenaGame/Heroes/Viking.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Heroes
+{
+    public class Viking : Hero
+    {
+        public int Berserk { get; private set; } = 0;
+        public Viking(string name, double armor, double strenght, IWeapon weapon) : base(name, armor, strenght, weapon)
+        {
+        }
+
+        public override double Attack()
+        {
+            double damage = base.Attack();
+            if (Berserk == 0)
+            {
+
+                double probability = random.NextDouble();
+                if (probability < 0.30)
+                {
+                    Berserk += 2;
+                }
+                return damage;
+
+            }
+
+            else
+            {
+                Berserk--;
+                return damage * 3;
+            }
+        }
+
+        public override double Defend(double damage)
+        {
+            if (Berserk == 0)
+            {
+                return base.Defend(damage);
+            }
+
+            else
+            {
+                Berserk--;
+                return base.Defend(damage) * 2;
+            }
+
+        }
+    }
+}
